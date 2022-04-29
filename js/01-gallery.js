bce90882375d1c9e897d477573fc0349382be129
Kate Yankovenko <k.yankovenko@ukr.net>
2022-04-29 18:37:52 +0200

    rendering gallery
    rendering gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const cardsGallery = document.querySelector(".gallery");
+const cardsMarkup = createPictureSetMarup(galleryItems);
+
+cardsGallery.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createPictureSetMarup(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return (
+            `
+    <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+        );
+    }).join('');
+}
+
 console.log(galleryItems);
 
 
